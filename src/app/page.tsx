1f5715f31ac47a0b2cea2be6d9a11187285dc017
Franco Giannoni <27508976+fagiannoni@users.noreply.github.com>
2024-11-23 12:55:11 -0300

    chore: create loading bar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
+"use client";
+
+import LoadingBar from "@/components/LoadingBar";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [progress, setProgress] = useState(0);
+  const maxProgress = 8;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress < maxProgress ? prevProgress + 1 : prevProgress
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [maxProgress]);
+
   return (
     <div>
-      <h1 className="text-[#878787]" >Hola</h1>
-
+      <h1 className="text-[#878787]">Hola</h1>
+      <div className="p-4">
+        <LoadingBar progress={progress} maxProgress={maxProgress} />
+      </div>
     </div>
   );
 }
